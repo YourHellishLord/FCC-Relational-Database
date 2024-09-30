1fe52e3a1bd5ee7d32e2467467ea76ee579b352a
Mauro De Rosa <it.mauro.derosa@gmail.com>
2024-09-30 18:46:55 +0200

    Learn-Relational-Databases-by-Building-a-Mario-Database.sql (part 2)

--- Learn-Relational-Databases-by-Building-a-Mario-Database.sql
+++ Learn-Relational-Databases-by-Building-a-Mario-Database.sql
@@ -27,8 +27,33 @@ ALTER TABLE second_table ADD COLUMN name VARCHAR(30);
 ALTER TABLE second_table RENAME COLUMN name TO username;
 -- \d second_table
 INSERT INTO second_table(id, username) VALUES(1, 'Samus');
-SELECT * From second_table;
+SELECT * FROM second_table;
 INSERT INTO second_table(id, username) VALUES(2, 'Mario');
-SELECT * From second_table;
+SELECT * FROM second_table;
 INSERT INTO second_table(id, username) VALUES(3, 'Luigi');
-SELECT * From second_table;
+SELECT * FROM second_table;
+DELETE FROM second_table WHERE username='Luigi';
+SELECT * FROM second_table;
+DELETE FROM second_table WHERE username='Mario';
+DELETE FROM second_table WHERE username='Samus';
+SELECT * FROM second_table;
+-- \d second_table
+ALTER TABLE second_table DROP COLUMN username;
+ALTER TABLE second_table DROP COLUMN id;
+-- \d
+DROP TABLE second_table;
+DROP TABLE first_table;
+-- \l
+ALTER DATABASE first_database RENAME TO mario_database;
+-- \l
+-- \c mario_database
+DROP DATABASE second_database;
+-- \l
+-- \d
+CREATE TABLE characters();
+ALTER TABLE characters ADD COLUMN character_id SERIAL;
+-- \d characters
+ALTER TABLE characters ADD COLUMN name VARCHAR(30) NOT NULL;
+ALTER TABLE characters ADD COLUMN homeland VARCHAR(60);
+ALTER TABLE characters ADD COLUMN favorite_color VARCHAR(30);
+-- \d characters

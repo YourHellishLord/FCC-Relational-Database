c5ab5818d6326f1ca0a916f3d5a6375a9b0bd3f1
Mauro De Rosa <it.mauro.derosa@gmail.com>
2024-10-24 17:23:30 +0200

    completed exercise learn git by building a sql reference object

---
+++ learn-git-sql-reference-object/learn_git.sh
@@ -0,0 +1,260 @@
+echo hello git
+mkdir sql_reference
+cd sql_reference/
+git init
+ls -a
+git status
+git checkout -b main
+git status
+touch README.md
+git status
+git add rea
+git add README.md 
+git status
+touch sql_reference.json
+git status
+git add sql_reference.json 
+git status
+git commit -m 'Initial commit'
+git status
+git log
+git status
+git diff
+git add sql_reference.json 
+git status
+git commit -m 'feat: add create database reference'
+git commit -m 'feat: add create database reference'
+git log
+git status
+git diff
+git add sql_reference.json 
+git commit -m "feat: add drop database reference"
+git log
+git branch
+git branch feat/add-create-table-reference
+git branch
+git checkout feat/add-create-table-reference 
+git branch 
+git status
+git diff
+git diff
+git status
+cd sql_reference/
+ls
+git status
+git add ./*
+git log
+git commit -m 'feat: add create table reference'
+git log
+git log --oneline
+git checkout main 
+git log --oneline
+git branch 
+git merge feat/add-create-table-reference 
+git log --oneline 
+git branch -d feat/add-create-table-reference 
+git branch 
+git branches
+git branch
+git checkout -b feat/add-drop-table-reference
+git status
+git diff
+git add sql_reference.json 
+git commit -m 'feat: add drop table reference'
+git checkout main 
+git branch
+git merge feat/add-drop-table-reference 
+git branch -d feat/add-drop-table-reference 
+git checkout -b feat/add-column-references
+git diff
+git add -
+git add .
+git commit -m 'feat: add column reference'
+git log --oneline
+git checkout main
+git checkout -b fix/create-table-syntax
+git status
+git diff
+git add .
+git commit -m 'fix: create table syntax'
+git checkout main 
+git branch
+git merge fix/create-table-syntax 
+git log --oneline
+git branch -d fix/create-table-syntax 
+git checkout main
+git checkout feat/add-column-references 
+git log
+git log --oneline
+git rebase main 
+git status
+git log --oneline
+git status
+git diff
+git add .
+git commit -m 'feat: add drop column reference'
+git log --oneline
+git checkout main 
+git checkout -b feat/add-insert-row-reference
+git status
+git log
+git log --oneline
+git diff
+cd sql_reference/
+git status
+git log
+git diff
+git add .
+git commit -m 'feat: add insert row reference'
+git checkout main
+git branches
+git branch
+git merge feat/add-insert-row-reference 
+git logs --oneline
+git log --oneline
+git checkout feat/add-column-references 
+git rebase main 
+git status
+git add sql_reference.json 
+git status
+git rebase --continue 
+git log --oneline
+git status
+git diff
+git add .
+git commit -m 'feat: add rename column reference'
+git checkout feat/add-insert-row-reference 
+git status
+git stash
+git status
+git stash list
+git stash pop
+git stash list
+git stash
+git stash list
+git stash show
+git stash show -p
+git stash apply
+git status
+git stash list
+git stash
+git stash list
+git stash show stash@{1}
+git stash show stash@{1} -p
+git stash drop stash@{1}
+git status
+git stash list
+git checkout main 
+git branch -d feat/add-insert-row-reference 
+git checkout -b feat/add-more-row-references
+git stash list
+git stash pop
+git stash list
+git diff
+git add .
+git status
+git commit -m 'feat: add update row reference'
+git checkout main 
+git merge feat/add-more-row-references 
+git checkout feat/add-column-references 
+git rebase main
+git status
+git add .
+git status
+git rebase --continue
+git log --oneline
+git diff
+git add .
+git commit -m 'feat: add primary key reference'
+git diff
+git add .
+git commit -m 'feat: add foreign key reference'
+git checkout feat/add-more-row-references 
+git diff
+git add .
+git commit -m 'feat: add delete row reference'
+git checkout main
+git merge feat/add-more-row-references 
+git branch -d feat/add-more-row-references 
+git checkout -b fix/add-missing-rename-references
+git diff
+git add .
+git commit -m 'fix: add missing rename database reference'
+git checkout feat/add-column-references 
+git rebase main 
+git status
+git add .
+git status
+git rebase --continue 
+git diff
+git add .
+git commit -m 'feat: add unique reference'
+git reset HEAD~1
+git log --oneline
+git status
+git diff
+git add .
+git commit -m 'feat: add unique reference'
+git revert HEAD
+git log --oneline
+git show
+git show HEAD~1
+git rebase --interactive HEAD~2
+git rebase --interactive HEAD~2
+git log --oneline
+git rebase --interactive --root
+git log --one-line
+git log --oneline
+git log --oneline
+git rebase main
+git log --oneline
+git rebase HEAD~5
+git rebase --interactive HEAD~5
+git log --oneline
+git log -1
+git checkout main
+git merge feat/add-column-references 
+git branch -d feat/add-column-references 
+git checkout fix/add-missing-rename-references 
+git rebase
+git rebase main 
+git log -1
+git log -5 --oneline
+git diff
+git add .
+git commit -m 'fix: add missing rename table reference'
+git log -5 --oneline
+git rebase --interactive HEAD~2
+git log -1 --oneline
+git log -1
+git checkout main 
+git merge fix/add-missing-rename-references 
+git branch
+git branch -d fix/add-missing-rename-references 
+git log --oneline
+git checkout -b feat/add-gitignore
+touch .env
+git status
+touch .gitignore
+git status
+git status
+git add .
+git commit -m 'feat: add .gitignore'
+touch sample.env
+git status
+git add .
+git commit -m 'feat: add sample.env'
+git log -5 --oneline
+git rebase --interactive HEAD~2
+git -1
+git log -1
+git checkout main 
+git merge feat/add-gitignore 
+git branch -d feat/add-gitignore 
+git log --oneline
+git log
+history
+history --help
+history -w
+cat ~/.bash_history
+history > learngit.bash
